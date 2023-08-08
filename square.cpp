e32f4dc01bfa3c8b2d0b1619961832c90724cd81
shantoasikur <141675776+shantoasikur@users.noreply.github.com>
2023-08-09 01:39:18 +0600

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+long long t;
+cin>>t;
+while(t--){
+    long long n;
+    long long s;
+
+    cin>>n>>s;
+
+    cout<<(s/(n*n))<<endl;
+
+
+
+
+
+}
+
+
+}
